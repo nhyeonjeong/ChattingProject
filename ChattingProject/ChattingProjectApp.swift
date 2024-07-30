4d37b3ef1f76f3b3256f1f9ef0b43754eb9f6e5e
bami <hjnam5962@gmail.com>
2024-07-30 14:21:54 +0900

    Initial Commit

---
+++ ChattingProject/ChattingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingProjectApp.swift
+//  ChattingProject
+//
+//  Created by 남현정 on 2024/07/30.
+//
+
+import SwiftUI
+
+@main
+struct ChattingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
